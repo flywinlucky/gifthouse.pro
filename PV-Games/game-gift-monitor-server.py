f774b00c8efada7a1478f2a1f938a1fd002c6543
Traffic Wolf <flywinlucky777@gmail.com>
2025-03-16 02:18:07 +0200

    Update game-gift-monitor-server.py

--- PV-Games/game-gift-monitor-server.py
+++ PV-Games/game-gift-monitor-server.py
@@ -6,6 +6,7 @@ from telethon import events
 api_id = '23597052'  # Replace with your api_id
 api_hash = 'd3481a88223f602c6cb3f0757daff693'  # Replace with your api_hash
 phone_number = '+37360535689'  # Replace with your phone number
+bot_username = '@gamegiftorders_bot'  # Replace with your bot's username
 
 # Main function to keep the script running as a server
 def main():
@@ -15,10 +16,11 @@ def main():
 
         print("Telegram client is running. Listening for events...")
 
-        # Example event handler (can be customized as needed)
-        @client.on(events.NewMessage)
+        # Event handler for new messages in the bot
+        @client.on(events.NewMessage(chats=bot_username))
         async def handle_new_message(event):
-            print(f"New message received: {event.message.text}")
+            print("New order received!")
+            print(f"Message content: {event.message.text}")
 
         # Keep the client running
         client.run_until_disconnected()
